3d54379b1c68f5bbebbae3ac343c4890f2d05031
amiruljack <amirularifar@gmail.com>
2022-09-18 13:41:59 +0800

    ts

---
+++ lib/country_reverse_geocoding.d.ts
@@ -0,0 +1,9 @@
+/**
+ * Reverse geocoding for countries
+ *
+ * @param spec { }
+ */
+export declare const countryReverseGeocoding: (lat: number, lng: number) => {
+    code: any;
+    name: any;
+} | null;
